8309cbaa5a79d424c1382996813e9f7838ac43a7
PancakeReaper <25124648+PancakeReaper@users.noreply.github.com>
2017-12-18 12:57:24 -0800

    Changed to create a list with inputs, instead of directly getting from gui for faster computing

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,17 +1,21 @@
 import tkinter as tk 
+import time
 
 window = tk.Tk()
 
 mainFrame = tk.Frame(window)
 
 board = []
+numbers = []
 
 # Automatically generate the empty sudoku board
 for row in range(9):
 	board.append([])
+	numbers.append([])
 	for col in range(9):
 		board[row].append(tk.Entry(window, width=3, justify='center'))
 		board[row][col].grid(row=row, column=col)
+		numbers[row].append(0)
 			
 # Solve Button
 solveButton = tk.Button(window, text=" Solve! ")
@@ -22,21 +26,21 @@ clearButton = tk.Button(window, text=" Clear! ")
 clearButton.grid(row=10, column=5, columnspan=3, pady=2)
 
 ## Returns first empty space on the board ##
-def findEmpty():
+def findEmpty(board):
 	for row in range(9):
 		for col in range(9):
-			if board[row][col].get() == "":
+			if board[row][col] == 0:
 				return [row, col]
 	return None
 
 ## Returns True if valid, else False ##
-def checkValid(row, col):
-	number = board[row][col].get()
+def checkValid(board, row, col):
+	number = board[row][col]
 	# Check if the same number exists in the same row or col
 	for i in range(9):
-		if i != row and board[i][col].get() == number:
+		if i != row and board[i][col] == number:
 			return False
-		if i != col and board[row][i].get() == number:
+		if i != col and board[row][i] == number:
 			return False
 	# Check if the same number exists in the same 3x3 box
 	b_row = row
@@ -48,7 +52,7 @@ def checkValid(row, col):
 	for x in range(3):
 		for y in range(3):
 			if b_row + y != row and b_col + x != col:
-				if board[b_row + y][b_col + x].get() == number:
+				if board[b_row + y][b_col + x] == number:
 					return False
 	return True
 
@@ -56,35 +60,56 @@ def checkValid(row, col):
 # Try 1 - 9 
 # Check if valid
 # Recurse and find next empty space
-def solve(event):
+def solve(board):
 
-	empty = findEmpty()
+	empty = findEmpty(board)
 	if empty != None:
 		row, col = empty
 	else:
-		"We're done!"
 		return
 
 	for attempt in range(1,10):
-		board[row][col].insert(0, str(attempt))
+		board[row][col] = attempt
 
-		if checkValid(row, col):
-			solve(event)
-			check = findEmpty()
+		if checkValid(board, row, col):
+			solve(board)
+			check = findEmpty(board)
 			if check == None:
-				"We're Done!"
+				# Check if there are any empty spaces left
 				return
 		# Not valid, empty out the board and try again
-		board[row][col].delete(0, 'end')
+		board[row][col] = 0
 	# 1 - 9 didn't work, backtrack it. 
 	return
+
+def solveHelp(event):
+	starttime = time.clock()
+
+	for row in range(9):
+		for col in range(9):
+			try:
+				i = int(board[row][col].get())
+			except:
+				i = 0
+			numbers[row][col] = i
+			board[row][col].delete(0, 'end')
+
+	solve(numbers)
+
+	for row in range(9):
+		for col in range(9):
+			board[row][col].insert(0, str(numbers[row][col]))
+
+	endtime = time.clock()
+	print("Time elapsed: " + str(endtime - starttime) + "s")
+
 
 def clear(event):
 	for y in range(9):
 		for x in range(9):
 			board[y][x].delete(0, 'end')
 
-solveButton.bind("<Button-1>", solve)
+solveButton.bind("<Button-1>", solveHelp)
 clearButton.bind("<Button-1>", clear)
 
 # Mainloop keeps the window up
